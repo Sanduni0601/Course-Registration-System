30f71decf7c5fc11dce80e8b72033588b35d97cd
Adithyagunasekara2000 <139615785+Adithyagunasekara2000@users.noreply.github.com>
2024-06-21 17:43:30 +0530

    Update User.js

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -1,1 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const UserController = require('../controller/userController');
+const { authenticateUser } = require('../middleware/auth');
 
+// Routes accessible to both students and admin
+router.post('/register', UserController.register);
+router.post('/login', UserController.login);
+router.post('/logout', UserController.logout);
+
+router.post('/register-course', authenticateUser, UserController.registerForCourse);
+
+module.exports = router;
