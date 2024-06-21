3be425502fec35d3021d5fcc1335067947628328
Adithyagunasekara2000 <139615785+Adithyagunasekara2000@users.noreply.github.com>
2024-06-21 17:39:27 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,1 +1,38 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
 
+const usersRouter = require('./routes/users');
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+
+const mongodbURL="mongodb+srv://thathsaraniadithyaa:2009Adith@socialconnect.ykuwxon.mongodb.net/Coursedb?retryWrites=true&w=majority&appName=socialConnect";
+
+mongoose.connect(mongodbURL,
+    {UseNewUrlParser: true,
+    useUnifiedTopology: true
+    
+    });
+
+    const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB connected");
+});
+
+app.use(session({
+  secret: 'project', 
+  resave: false,
+  saveUninitialized: true,
+  store: MongoStore.create({ mongoUrl: mongodbURL })
+}));
+
+app.use('/api/users', usersRouter);
+
+const port = process.env.PORT || 3045;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
